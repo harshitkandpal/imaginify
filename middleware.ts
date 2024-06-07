535c99c9a24db3f9d8511e23449d4a4f697a1305
Harshit Kandpal <harshitheroh5@gmail.com>
2024-06-07 11:12:40 +0530

    database added

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,20 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const publicRoutes = ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'];
+
+export default async function middleware(req: NextRequest, event: NextFetchEvent) {
+  const url = req.nextUrl.clone();
+  const path = url.pathname;
+
+  if (publicRoutes.includes(path)) {
+    return NextResponse.next();
+  }
+
+  // Use Clerk's middleware for other routes
+  return clerkMiddleware()(req, event);
+}
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
