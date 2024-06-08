70f1ac9d6853c40cc6e6e9ec4c1e8b8efc6d4778
Harshit Kandpal <harshitheroh5@gmail.com>
2024-06-08 20:33:27 +0530

    resolved err clerk

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,10 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { clerkMiddleware } from '@clerk/nextjs/server';
-
-const publicRoutes = ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'];
-
-export default async function middleware(req: NextRequest, event: NextFetchEvent) {
-  const url = req.nextUrl.clone();
-  const path = url.pathname;
-
-  if (publicRoutes.includes(path)) {
-    return NextResponse.next();
-  }
-
-  // Use Clerk's middleware for other routes
-  return clerkMiddleware()(req, event);
-}
-
+ 
+export default clerkMiddleware()
+ 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
+    '/', // Run middleware on index page
+    '/(api|trpc)(.*)'], // Run middleware on API routes
 };
